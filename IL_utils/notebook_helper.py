41f4aa2e9593d259619a21c48cc11827cfef3c87
EonianCoda <textuseful159000@gmail.com>
2021-09-16 12:41:58 +0800

    add text to args

--- IL_utils/notebook_helper.py
+++ IL_utils/notebook_helper.py
@@ -1,5 +1,17 @@
 import argparse
 
+def text_to_args(args):
+    """convert text to args, in order to create Params
+    """
+    args = [arg.rstrip() for arg in args.split('--') if arg != '']
+    result_arg = []
+    for arg in args:
+        texts = arg.split(' ')
+        result_arg.append('--' + texts[0])
+        for i in range(1, len(texts)):
+            result_arg.append(texts[i])
+    return result_arg
+
 def parse_opt(args=None):
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', type=str, default='yolov5s.pt', help='initial weights path')
